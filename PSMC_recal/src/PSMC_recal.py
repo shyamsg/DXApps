64f54331aec24ab0c3487497024c5956d544a016
shyamsg <shyam@uchicago>
2013-04-03 18:59:56 -0500

    Changed the recal script

--- PSMC_recal/src/PSMC_recal.py
+++ PSMC_recal/src/PSMC_recal.py
@@ -14,24 +14,115 @@
 import os
 import dxpy
 import subprocess
+import numpy as np
+
+def genTR(fname):
+    """This function generates ntot, nfree, pattern and theta/rho 
+    vectors.
+    """
+    nfree = 0
+    ntot = 0
+    pattern = ''
+    thetas = 0
+    rhos = 0
+    f = os.popen('grep pattern '+fname)
+    line = f.readline()
+    line = line.strip()
+    toks = line.split()
+    currpattern = (toks[1].split(':')[1])[0:-1]
+    if (pattern == ''):
+        pattern = currpattern
+    elif (pattern != currpattern):
+        raise Exception('Pattern not same for all input files.\n')
+    currntot = int((toks[2].split(':')[1])[0:-1])
+    if (ntot == 0):
+        ntot = currntot
+    elif (ntot != currntot):
+        raise Exception('Total parameters not same for all input files.\n')
+    currnfree = int((toks[3].split(':')[1]))
+    if (nfree == 0):
+        nfree = currnfree
+    elif (nfree != currnfree):
+        raise Exception('Free parameters not same for all input files.\n')
+    f.close()
+    cmd = 'tail -n 2 '+fname+' | head -1'
+    f = os.popen(cmd)
+    line = f.readline()
+    line = line.strip()
+    toks = line.split()
+    thetas = float(toks[2])
+    rhos = float(toks[3])
+    currLs = []
+    for ll in xrange(nfree):
+        currLs.append(float(toks[4+ll]))
+    f.close()
+    return (pattern, ntot, nfree, thetas, rhos, currLs)
+
+def gettimes(tmax_orig, tmax_new):
+    """
+    This method returns the time boundaries.
+    In this case, it is linear with 5ky lengths
+    in the near past and 10ky lengths earlier
+    and so on.
+    In detail:
+    0-100KYA:    20*5KY   -> 20*0.01 = 0.2
+    100-250KYA:  15*10KY  -> 15*0.02 = 0.3
+    250-500KYA:  10*25KY  -> 10*0.05 = 0.5
+    500-1MYA:    5*100KY  -> 5*0.2   = 1
+    1MYA-7.5MYA: 13*500KY -> 13*1    = 13
+    The final window that exists from 7.5MYA to infinite time back.
+    """
+    times = np.ones(63)
+    times[0:20] = times[0:20]*0.01
+    times[20:35] = times[20:35]*0.02
+    times[35:45] = times[35:45]*0.05
+    times[45:50] = times[45:50]*0.2
+    times[50:] = times[50:]*1
+    times = times.cumsum()
+    times = times*tmax_new/tmax_orig
+    return times
+
+def writeRecalFile(fname, maxtime, window, xChr):
+    maxtime=7.5e6
+    if (xChr):
+        mu = 2.2e-8
+    else:
+        mu = 2.5e-8
+    gtime = 25
+    tmax_orig = maxtime/(2e4*gtime)
+    (pattern, ntot, nfree, thetas, rhos, Ls, fnames) = genTR(fname)
+    tmaxNew = 4*maxtime*window*mu/(2*gtime*thetas)
+    fntemp = fname+'temp2.pars'
+    outf = open(fntemp, 'w')
+    outf.write('3*2+52*1+6\n')
+    outf.write(str(thetas)+'\n')
+    outf.write(str(rhos)+'\n')
+    ts = gettimes(tmax_orig, tmaxNew)
+    for lllo in range(56):
+        outf.write('1.0000\n')
+    outf.write('\n'.join([str(x) for x in ts]))
+    outf.write('\n')
+    outf.close()
+    return (tmaxNew, fntemp)
 
 @dxpy.entry_point('main')
-def main(psmcfa, outname, timemax, parfile):
+def main(psmcfa, psmc, outname, xchr, timemax, window):
 
     # The following line(s) initialize your data object inputs on the platform
     # into dxpy.DXDataObject instances that you can start using immediately.
 
     psmcfa = dxpy.DXFile(psmcfa)
-    parfile = dxpy.DXFile(parfile)
+    psmc = dxpy.DXFile(psmc)
 
     # The following line(s) download your file inputs to the local file system
     # using variable names for the filenames.
 
     dxpy.download_dxfile(psmcfa.get_id(), "psmcfa")
-    dxpy.download_dxfile(parfile.get_id(), "parfile")
+    dxpy.download_dxfile(psmc.get_id(), "psmc")
 
     # Fill in your application code here.
-    subprocess.check_call(['psmc', '-t', str(round(timemax,4)), '-i', 'parfile', '-o', outname, 'psmcfa'])
+    (tmaxNew, parfile) = writeRecalFile(psmc, timemax, window, xchr)
+    subprocess.check_call(['psmc', '-t', str(round(tmaxNew,4)), '-i', parfile, '-o', outname, 'psmcfa'])
     # The following line(s) use the Python bindings to upload your file outputs
     # after you have created them on the local file system.  It assumes that you
     # have used the output field name for the filename for each output, but you
